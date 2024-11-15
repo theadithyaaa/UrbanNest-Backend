77cd3b07c1beb0abe010390c69acaaf67e271419
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-15 13:52:42 +0530

    login fixed

--- src/main/java/edu/icet/controller/LoginController.java
+++ src/main/java/edu/icet/controller/LoginController.java
@@ -3,6 +3,7 @@ package edu.icet.controller;
 import edu.icet.dto.Login;
 import edu.icet.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
+@Slf4j
 public class LoginController {
     @Autowired
     final LoginService service;
@@ -25,6 +27,7 @@ public class LoginController {
     @PostMapping("/add-login")
     @ResponseStatus(HttpStatus.CREATED)
     public void addLogin(@RequestBody Login login){
+        log.info("updated-> {}",login);
         service.addLogin(login);
     }
 }
--- src/main/java/edu/icet/entity/LoginEntity.java
+++ src/main/java/edu/icet/entity/LoginEntity.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Table(name = "login")
 public class LoginEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
     private String email;
