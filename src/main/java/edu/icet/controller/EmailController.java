137c70e2a4c919236d188097d04da0a3bc1cfdd7
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-20 14:12:51 +0530

    email send

---
+++ src/main/java/edu/icet/controller/EmailController.java
@@ -0,0 +1,23 @@
+package edu.icet.controller;
+
+import edu.icet.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
+        emailService.sendEmail(to, subject, text);
+        return "Email sent successfully!";
+    }
+}
---
+++ src/main/java/edu/icet/service/EmailService.java
@@ -0,0 +1,5 @@
+package edu.icet.service;
+
+public interface EmailService {
+    void sendEmail(String to, String subject, String text);
+}
