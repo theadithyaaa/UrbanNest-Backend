b29f81cbc880791d689acd4ff37811803805c10e
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-21 21:50:45 +0530

    updated

--- src/main/java/edu/icet/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.repository;
-
-import edu.icet.entity.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<CustomerEntity,Integer> {
-}
